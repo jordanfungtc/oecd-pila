8afcb532506fc154e3e58550e7d89977a8c0b693
Jordan Fung <jordanfung2002@gmail.com>
2024-11-25 15:57:18 +0800

    Refactor dashboard code

---
+++ src/util/DbVertical.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DbAnswer from "./DbAnswer.vue";
+const props = defineProps(["states", "questions"]);
+</script>
+
+<template>
+  <table class="table table-sm w-full my-0">
+    <tbody>
+      <tr v-for="(question, key) in props.questions" :key="key">
+        <th>{{ key }}</th>
+        <td class="w-full">
+          <DbAnswer :states="props.states" :question="question" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
