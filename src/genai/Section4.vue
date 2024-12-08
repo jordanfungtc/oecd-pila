306b1003435e3debbc4ef52d393576108f600776
Jordan Fung <jordanfung2002@gmail.com>
2024-12-08 23:31:25 +0800

    Congrats ending

--- src/genai/Section4.vue
+++ src/genai/Section4.vue
@@ -11,12 +11,13 @@ import SustainabilityInference from "./s4/SustainabilityInference.vue";
 import CaseSelect from "./s4/CaseSelect.vue";
 import CaseProsCons from "./s4/CaseProsCons.vue";
 import CaseWorth from "./s4/CaseWorth.vue";
+import Ending from "./s4/Ending.vue";
 
 store.title = "Generative AI Module";
 </script>
 
 <template>
-  <Frame :steps="10">
+  <Frame :steps="11">
     <template #0><Intro /></template>
     <template #1><BiasEmbeddings /></template>
     <template #2><BiasLlm /></template>
@@ -27,5 +28,6 @@ store.title = "Generative AI Module";
     <template #7><CaseSelect /></template>
     <template #8><CaseProsCons /></template>
     <template #9><CaseWorth /></template>
+    <template #10><Ending /></template>
   </Frame>
 </template>
